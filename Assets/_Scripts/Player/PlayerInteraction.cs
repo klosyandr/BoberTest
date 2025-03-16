1ecab6b94b5e03a3f0c441079065b7f48174fbd4
klosyandr <klosyandr@gmail.com>
2025-03-17 01:31:32 +0800

    [dev] OutLine for interactable

--- Assets/_Scripts/Player/PlayerInteraction.cs
+++ Assets/_Scripts/Player/PlayerInteraction.cs
@@ -6,6 +6,9 @@ public class PlayerInteraction : MonoBehaviour
     [Header("Interaction Settings")]
     [SerializeField] private LayerMask _layer;
     [SerializeField] private float _distance = 3f;
+    
+    private GameObject _lastHighlighted;
+    private Outline _lastOutline;
 
     private void Awake()
     {
@@ -13,20 +16,53 @@ public class PlayerInteraction : MonoBehaviour
         _player.Input.OnInteract += OnInteract;
     }
 
-    private void OnInteract(Vector2 vector)
+    private void Update()
     {
         Ray ray = new Ray(_player.Camera.transform.position, _player.Camera.transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction * _distance);
 
+#if UNITY_EDITOR
+Debug.DrawRay(ray.origin, ray.direction * _distance);
+#endif
+        
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, _distance, _layer))
+        if (Physics.Raycast(ray, out hit, _distance, _layer))
         {
-            var target = hit.collider.gameObject.GetComponent<IInteractableComponent>();
-            if (target != null)
+            var target = hit.collider.gameObject;
+
+            if (_lastHighlighted != target)
             {
-                target.Interact(_player);
+                ClearOutline();
+
+                _lastHighlighted = target;
+                _lastOutline = _lastHighlighted.GetComponent<Outline>();
+
+                if (_lastOutline != null)
+                {
+                    _lastOutline.enabled = true;
+                }
             }
         }
+        else
+        {
+            ClearOutline();
+        }
+    }
+
+    private void OnInteract(Vector2 vector)
+    {
+        var target = _lastHighlighted?.GetComponent<IInteractableComponent>();
+        target?.Interact(_player);
+    }
+
+    private void ClearOutline()
+    {
+        if (_lastOutline != null)
+        {
+            _lastOutline.enabled = false;
+        }
+
+        _lastHighlighted = null;
+        _lastOutline = null;
     }
 
     private void OnDestroy()
